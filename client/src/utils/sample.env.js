803d15c9bc40942af95b8a3005ab51cb60161d81
Rohit <jambhalerohit07@gmail.com>
2026-07-14 16:15:23 +0530

    change in mongoURI cred

--- client/src/utils/sample.env.js
+++ client/src/utils/sample.env.js
@@ -1,7 +1,6 @@
 PORT = "4001";
 //  MONGODB_URI="mongodb+srv://jambhalerohit07_db_user:UD2VnlU4rSOmNiSk@cluster0.fhba2rl.mongodb.net/"
-MONGODB_URI =
-  "mongodb+srv://jambhalerohit07_db_user:Rohit123@cluster0.fhba2rl.mongodb.net/";
+MONGODB_URI = "mongodb+srv://j******:Roh*****@cluster0.fhba2rl.mongodb.net/";
 ACCESS_TOKEN_SECRET =
   "3f0c7f7e9fbd8ee41a5a2ce0cbb42e1abf5b76cc2d2ea779d542c9e928b3db54";
 REFRESH_TOKEN_SECRET =
